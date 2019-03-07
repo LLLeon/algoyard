fcd5cf70ce38e03aa186a7214720f96677b12deb
LLLeon <jiachenhui1989@gmail.com>
2019-03-08 01:08:50 +0800

    sorting: update quick sort annotation

--- algorithm/sorting/quick/quick.go
+++ algorithm/sorting/quick/quick.go
@@ -1,5 +1,7 @@
 package quick
 
+import "fmt"
+
 // QuickSort implements quick sort.
 //
 // Depending on whether the partition is balanced or not,
@@ -22,25 +24,38 @@ func separateSort(list []int, start, end int) {
 		return
 	}
 
-	i := partition(list, start, end)
-	separateSort(list, start, i-1)
-	separateSort(list, i+1, end)
+	pivotIndex := partition(list, start, end)
+	separateSort(list, start, pivotIndex-1)
+	separateSort(list, pivotIndex+1, end)
 }
 
 func partition(list []int, start, end int) int {
 	pivot := list[end]
 	i := start
 
+	fmt.Printf("original list: %v\n", list)
+	fmt.Printf("pivot: %d\n", pivot)
+
 	for j := start; j < end; j++ {
+		fmt.Printf("enter loop, i = %d, j = %d\n", i, j)
 		if list[j] < pivot {
 			if i != j {
 				list[i], list[j] = list[j], list[i]
+				fmt.Printf("exchanged list[%d] <---> list[%d]\n", i, j)
 			}
 			i++
 		}
+		fmt.Printf("exit loop, i = %d, j = %d\n", i, j)
+		fmt.Printf("\n")
 	}
 
+	// exchange the value of i with pivot
 	list[i], list[end] = list[end], list[i]
 
+	fmt.Printf("list[%d] <---> list[%d]\n", i, end)
+	fmt.Printf("changed list: %v\n", list)
+	fmt.Printf("\n")
+
+	// returns the index of pivot
 	return i
 }
--- algorithm/sorting/quick/quick_test.go
+++ algorithm/sorting/quick/quick_test.go
@@ -3,7 +3,7 @@ package quick
 import "testing"
 
 func TestQuickSort(t *testing.T) {
-	list := []int{6, 5, 4, 3, 2, 1}
+	list := []int{5, 7, 3, 8, 4, 6}
 	t.Log("Original list:", list)
 	QuickSort(list, len(list))
 	t.Log("Sorting result:", list)
