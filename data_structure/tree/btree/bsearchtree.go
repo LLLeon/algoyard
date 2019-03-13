8afe9f0ec5d27634a2a580a8662e16bb39fb858a
LLLeon <jiachenhui1989@gmail.com>
2019-03-13 17:57:38 +0800

    tree: add binary search tree

---
+++ data_structure/tree/btree/bsearchtree.go
@@ -0,0 +1,113 @@
+package btree
+
+// Find implements a binary search tree search for specified data.
+func Find(root *Node, data int) *Node {
+	if root.data == data {
+		return root
+	}
+
+	p := root
+	for p != nil {
+		if data < p.data {
+			p = p.left
+		} else if data > p.data {
+			p = p.right
+		} else {
+			return p
+		}
+	}
+
+	return nil
+}
+
+// Insert the specified data into the binary search tree.
+func Insert(root *Node, data int) *Node {
+	if root == nil {
+		root = NewNode(data)
+		return root
+	}
+
+	p := root
+	for p != nil {
+		if data > p.data {
+			if p.right == nil {
+				p.right = NewNode(data)
+				return root
+			}
+			p = p.right
+		} else {
+			if p.left == nil {
+				p.left = NewNode(data)
+				return root
+			}
+			p = p.left
+		}
+	}
+
+	return root
+}
+
+// Delete implements the removal of specified data from
+// the binary search tree.
+func Delete(root *Node, data int) {
+	if root == nil {
+		return
+	}
+
+	var pp *Node // the parent node of p
+	p := root    // the Node to delete
+
+	// find the node to delete
+	for p != nil && p.data != data {
+		pp = p
+		if data > p.data {
+			p = p.right
+		} else {
+			p = p.left
+		}
+	}
+
+	// not found
+	if p == nil {
+		return
+	}
+
+	// the node to be deleted has two child nodes.
+	// find the smallest node in the right subtree.
+	if p.left != nil && p.right != nil {
+		minP := p.right
+		minPP := p
+
+		for minP.left != nil {
+			minPP = minP
+			minP = minP.left
+		}
+
+		// Replace the value of p with the value of minP.
+		// The node to delete becomes minP.
+		p.data = minP.data
+		p = minP
+		pp = minPP
+	}
+
+	// The node to be deleted is a leaf node or has only
+	// one child node.
+	var child *Node
+
+	if p.left != nil {
+		child = p.left
+	} else if p.right != nil {
+		child = p.right
+	} else {
+		child = nil
+	}
+
+	// The node to delete is the root node.
+	if pp == nil {
+		root = child
+	} else if pp.left == p {
+		pp.left = child
+	} else {
+		pp.right = child
+	}
+}
