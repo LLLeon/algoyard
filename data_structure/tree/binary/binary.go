33cfbd1be79f91b5e3991d53762acf55b72f4cef
LLLeon <jiachenhui1989@gmail.com>
2019-03-13 11:31:32 +0800

    tree: add traversal of binary tree

---
+++ data_structure/tree/binary/binary.go
@@ -0,0 +1,49 @@
+package binary
+
+import "fmt"
+
+// Node represents a node in a binary tree.
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+// PreOrder implements pre-order traversal of binary tree.
+// Each node is accessed up to twice,
+// time complexity is O(n).
+func PreOrder(root *Node) {
+	if root == nil {
+		return
+	}
+
+	fmt.Println(root.data)
+	PreOrder(root.left)
+	PreOrder(root.right)
+}
+
+// InOrder implements in-order traversal of binary tree.
+// Each node is accessed up to twice,
+// time complexity is O(n).
+func InOrder(root *Node) {
+	if root == nil {
+		return
+	}
+
+	InOrder(root.left)
+	fmt.Println(root.data)
+	InOrder(root.right)
+}
+
+// PostOrder implements post-order traversal of binary tree.
+// Each node is accessed up to twice,
+// time complexity is O(n).
+func PostOrder(root *Node) {
+	if root == nil {
+		return
+	}
+
+	PostOrder(root.left)
+	PostOrder(root.right)
+	fmt.Println(root.data)
+}
